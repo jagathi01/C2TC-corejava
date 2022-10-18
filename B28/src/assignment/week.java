7b7ebc1f46721ce1737ee2a2445f444b8150ed94
ADMIN <ADMIN@jagathisreenivas>
2022-10-18 19:46:55 +0530

    com.tns.oopsconcept

--- B28/src/assignment/week.java
+++ B28/src/assignment/week.java
@@ -1,5 +1,18 @@
-package assignment;
+package assignments;
 
 public class week {
 
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		 int temp=30;
+	     for(int week=1;week<=5;week++)
+	     {
+	      System.out.println("Week"+week+":");
+	      System.out.println("Temperature ="+temp+"degree");
+	      temp=temp-2;
+
+	     }
+
+	}
 }
---
+++ B28/src/com/tns/oopsconcept/Car.java
@@ -0,0 +1,5 @@
+package com.tns.oopsconcept;
+
+public class Car {
+
+}
